dab8126115cac78d1347daae457fd8290bf57cc8
valerijs <valerijs.dziemidovs@routedin.com>
2022-05-17 23:02:54 +0300

    added binance websocket, run.sh, websocket messages, possible to place and cancel orders etc.

---
+++ logger.py
@@ -0,0 +1,17 @@
+import logging
+
+logger = logging.getLogger()
+
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chmod +x run.sh
+
+python3 main.py
